0e355eefd87aa0773e85cf1ce4f4223d8634eecf
ozdemirozcelik <ozdemirozcelik@gmail.com>
2023-04-19 16:33:01 -0400

    more

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+
+    path(route='', view=views.get_dealerships, name='index'),
     
     # path for about view
     path(route='about/', view=views.about, name='about'),
@@ -16,12 +18,14 @@ urlpatterns = [
     path(route='contact/', view=views.contact, name='contact'),
 
     # path for registration
+    path('registration/', views.registration_request, name='registration'),
 
     # path for login
+    path('login/', views.login_request, name='login'),
 
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
 
-    path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
